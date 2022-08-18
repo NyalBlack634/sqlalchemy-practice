95c0c2bd43617d8d127e8ab9666939bc00f325d7
NyalBlack634 <Ke7o9.Nyal@gmail.com>
2022-08-18 19:40:21 +0900

    複数キーワード検索

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine
+from sqlalchemy import or_
 from sqlalchemy.ext.declarative import declarative_base
 # from sqlalchemy.orm import relationship
 from sqlalchemy.orm import sessionmaker
@@ -40,12 +41,42 @@ class Headline(Base):
     text = Column(String)
 
 
+def search_keyword(keyword: str, n: int):
+    """キーワード検索(キーワード１つの場合)
+
+    Args:
+        keyword (str): 検索キーワード
+        n (int): 引き出す見出し数
+
+    Returns:
+        _type_: _description_
+    """
+    return session.query(Headline).filter(Headline.text.like(f'%{keyword}%')).limit(n).all()
+
+
+def search_keywords(keywords: list, n: int):
+    """複数キーワードの部分一致
+
+    Args:
+        keywords (list): 検索キーワード
+        n (int): 引き出す見出し数
+
+    Returns:
+        _type_: _description_
+    """
+
+    filters = [Headline.text.like(f'%{keyword}%') for keyword in keywords]
+
+    return session.query(Headline).filter(or_(*filters)).limit(n).all()
+
+
 if __name__ == '__main__':
     # セッションの生成
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    headlines = session.query(Headline).limit(100).all()
+    # headlines = session.query(Headline).limit(100).all()
+    headlines = search_keywords(['別府'], 10)
 
     for headline in headlines:
         print(f'{headline.path}')
